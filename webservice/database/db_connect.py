8fca74f3b8fca07096f479db4a1d642c97991f37
jo052499 <johnatan.duarte@intelbras.com.br>
2021-03-17 01:41:18 -0300

    Criado interface e modulos de captura dos dados

---
+++ webservice/database/db_connect.py
@@ -0,0 +1,56 @@
+import sqlite3 as sql
+
+def create_db():
+    try:
+        conn = sql.connect('database.db')
+        conn.execute('CREATE TABLE tb_usuario (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL)')
+        print("Tabela criada corretamente")
+        conn.close()
+    except:
+        print("Conexão não pode ser sucedida")
+
+def drop_db():
+    try:
+        conn = sql.connect('database.db')
+        conn.execute('DROP TABLE tb_usuario')
+        print("Tabela deletada com sucesso!")
+        conn.close()
+    except:
+        print("Conexão negada")
+
+
+def add_user_and_passw(username, password):
+    try:
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("SELECT * FROM tb_usuario where username=?",[username])
+            con.row_factory = sql.Row
+            rows = cur.fetchall()
+            if rows:
+                print("Username já existente!")
+                return 0, False
+            else:
+                con.commit()
+                cur.execute("INSERT INTO tb_usuario (username,password)  VALUES (?,?)",(username,password))   
+                id = cur.lastrowid
+                print("Usuario criado, ID:",id)
+                return id, True
+    except:
+        print("Erro no cadastro")
+        
+
+def get_user_and_passw(id):
+    try:
+        con = sql.connect("database.db")
+        con.row_factory = sql.Row
+        cur = con.cursor()
+        cur.execute("select * from tb_usuario")
+   
+        rows = cur.fetchall()
+        username = rows[id]['username']
+        password = rows[id]['password']
+        return username, password
+    except:
+        print("Não foi possivel obter os usuarios!")
+
+drop_db()
--- webservice/server.py
+++ webservice/server.py
@@ -1,15 +1,56 @@
-from flask import Flask, render_template, redirect, request, url_for, jsonify
-
-
-
-
+from database.db_connect import drop_db, create_db, add_user_and_passw, get_user_and_passw 
+from flask import Flask, render_template, request, jsonify, current_app, g
+from flask.cli import with_appcontext
+import sqlite3
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.neighbors import KNeighborsClassifier
+from collections import Counter
+from sklearn.metrics import accuracy_score
+import pickle
+import glob
+import csv
+import numpy as np
+import pandas as pd
+import os
 
 app = Flask(__name__)
+create_db() # Cria banco SQL
 
 @app.route('/')
-def home() :
+def home():
 	return render_template('login.html')
 
+@app.route('/cadastro')
+def cadastro():
+	return render_template('cadastro.html')
+
+@app.route('/usuario', methods = ['GET', 'POST'])
+def usuario():
+	if request.method == 'POST':
+		response = dict(request.get_json())
+		username  = response['username']
+		password = response['password']
+		id, result = add_user_and_passw(username, password)
+
+		if result:
+			return jsonify({'cadastro_cod': 'UserRegistrySuccess', 'id_usuario': id})
+		else:
+			return jsonify({'cadastro_cod': 'UsernameAlreadyExist'})
+
+@app.route('/biometria', methods = ['POST'])
+def biometria():
+	if request.method == 'POST':
+		response = dict(request.get_json())
+		user_id  = response['user_id']
+		data = response['data']
+		dataframe = pd.DataFrame(data)
+		path = "biometic/" + str(user_id) + ".csv"
+		try: 
+			dataframe.to_csv(path, header=False, index=False)
+			return jsonify({'biometric_cod': 'Success'})
+		except:
+			print("Não foi possivel gerar o arquivo .csv")
 
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', debug=True, port=4000)
+	app.run(host='127.0.0.1', debug=True, port=3000)
+
