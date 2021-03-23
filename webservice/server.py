dea4c79cb228f695c465533028d890daf122c349
jo052499 <johnatan.duarte@intelbras.com.br>
2021-03-23 19:45:29 -0300

    Add - Tela de login

--- webservice/server.py
+++ webservice/server.py
@@ -1,4 +1,4 @@
-from database.db_connect import drop_db, create_db, add_user_and_passw, get_user_and_passw 
+from database.db_connect import drop_db, create_db, add_user_and_passw, check_user_and_passw
 from flask import Flask, render_template, request, jsonify, current_app, g
 from flask.cli import with_appcontext
 import sqlite3
@@ -14,6 +14,7 @@ import pandas as pd
 import os
 
 BIOMETRIA_PATH = '../sdk_lib/sdk/biometrias/' # Pasta onde será salvo os dados .csv
+drop_db() # Deleta o banco caso exista
 create_db() # Cria o banco de dados SQLite
 
 app = Flask(__name__)
@@ -52,6 +53,34 @@ def biometria():
 		except:
 			print("Não foi possivel gerar o arquivo .csv")
 
+@app.route('/login', methods = ['GET'])
+def login():
+	return render_template('./login/login.html')
+
+@app.route('/login/auth1', methods = ['POST']) # Rota para a primeira autenticação
+def auth1():
+	response = dict(request.get_json())
+	username  = response['username']
+	password = response['password']
+	print()
+
+	id, result, user_id = check_user_and_passw(username, password)
+
+	if result:
+		return jsonify({'auth1_code': 'success', 'id_usuario': user_id})
+	else:
+		if id == 3:
+			return jsonify({'auth1_code': 'UsernameNotExist'})
+		elif id == 1:
+			return jsonify({'auth1_code': 'PasswordIsWrong'})
+
+@app.route('/login/auth2', methods = ['POST']) # Rota para a primeira autenticação
+def auth2():
+	response = dict(request.get_json())
+	typing_data  = response['typing_data']
+	user_id = response['user_id']
+	print(typing_data)
+	print(user_id)
 
 
 # Server Start
