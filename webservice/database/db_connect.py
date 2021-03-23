cba841d4237f14ff57bf1488af965dac3fd0f58e
jo052499 <johnatan.duarte@intelbras.com.br>
2021-03-23 19:44:24 -0300

    Add - função de verificação de usuario e senh

--- webservice/database/db_connect.py
+++ webservice/database/db_connect.py
@@ -4,19 +4,19 @@ def create_db():
     try:
         conn = sql.connect('database.db')
         conn.execute('CREATE TABLE tb_usuario (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL)')
-        print("Tabela criada corretamente")
+        print("INFO - DATABASE - Tabela criada corretamente")
         conn.close()
     except:
-        print("Conexão não pode ser sucedida")
+        print("ERROR - DATABASE - Conexão não pode ser sucedida")
 
 def drop_db():
     try:
         conn = sql.connect('database.db')
         conn.execute('DROP TABLE tb_usuario')
-        print("Tabela deletada com sucesso!")
+        print("INFO - DATABASE - Tabela deletada com sucesso!")
         conn.close()
     except:
-        print("Conexão negada")
+        print("ERROR - DATABASE - Conexão negada ou banco não existe")
 
 
 def add_user_and_passw(username, password):
@@ -37,8 +37,31 @@ def add_user_and_passw(username, password):
                 return id, True
     except:
         print("Erro no cadastro")
-        
 
+def check_user_and_passw(username, password):
+    try:
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("SELECT * FROM tb_usuario where username=?",[username])
+            con.row_factory = sql.Row
+            rows = cur.fetchall()
+            if rows:
+                for row in rows:
+                    user_id = row[0]
+                    usuario = row[1]
+                    senha = row[2]
+                if senha == password:
+                    print('WARNING - DATABASE - check_user_and_pass: Usuario e senha conferem, usuario autenticado!  USER_ID:',user_id, 'USER_NAME:',usuario)
+                    return 2, True, user_id
+                else: 
+                    print('WARNING - DATABASE - check_user_and_pass: Usuario existente porem senha não confere! USER_ID:',user_id, 'USER_NAME:',usuario)
+                    return 1, False, user_id
+            else:
+                print("ERROR - DATABASE - check_user_and_pass: Usuario não existe no banco!")
+                return 3, False, 0
+    except:
+        print("ERROR - DATABASE - Não foi possivel verificar o usuario e senha")
+        
 def get_user_and_passw(id):
     try:
         con = sql.connect("database.db")
